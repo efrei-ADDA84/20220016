f547be5bda6eef549045541fa0d999016b06109d
Sarahhabbi <habbisarah.1@gmail.com>
2023-05-25 11:02:04 +0200

    change route

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/weather', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_weather():
     API_KEY = os.environ.get('API_KEY')
     lat = request.args.get('lat')
