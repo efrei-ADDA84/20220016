b3803b66bc5d3ea84ff6fffe1f21195ffccc8398
Sarahhabbi <habbisarah.1@gmail.com>
2023-05-25 11:56:22 +0200

    remove port

--- app.py
+++ app.py
@@ -25,4 +25,4 @@ def get_weather():
         return jsonify({'error': f'ERROR: {response.status_code}'})
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run()
